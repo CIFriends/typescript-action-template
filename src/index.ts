f34269dc470f1c63dcc7bb48837085cfe7b5ec11
Breno A. <git@breno.tech>
2024-05-04 13:53:47 -0300

    refactor: code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+/**
+ * The entrypoint for the action.
+ */
+import { run } from "./main";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet: string = core.getInput("who-to-greet");
-
-  console.log(`Hello ${nameToGreet}!`);
-
-  const time: string = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload: string = JSON.stringify(github.context.payload, undefined, 2);
-
-  console.log(`The event payload: ${payload}`);
-} catch (error: unknown) {
-  core.setFailed((error as Error).message);
-}
+void run();
