789b63bca9cfa110ce7742de9a51c0877b6117db
Breno A. <git@breno.tech>
2024-05-04 15:38:45 -0300

    refactor: sample file

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,17 @@ import * as core from "@actions/core";
 
 /**
  * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
+ * @returns {void} Resolves when the action is complete.
  */
-export async function run(): Promise<void> {
+export function run(): void {
   try {
-    const ms: string = core.getInput("milliseconds");
+    const who: string = core.getInput("who-to-greet");
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`);
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString());
-    await new Promise(resolve => setTimeout(resolve, parseInt(ms, 10)));
-    core.debug(new Date().toTimeString());
+    core.debug(`Greeting ${who}... at ${new Date().toTimeString()}`);
 
     // Set outputs for other workflow steps to use
-    core.setOutput("time", new Date().toTimeString());
+    core.setOutput("message", `Hello, ${who}!`);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message);
